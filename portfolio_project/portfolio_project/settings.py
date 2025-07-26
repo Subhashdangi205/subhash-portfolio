03b4f721f892e3988f02646520ef1e2461976e4b
subhash dangi <166481929+Subhashdangi205@users.noreply.github.com>
2025-07-26 10:48:50 +0530

    Fix: Vercel deployment config

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR / 'portfolio' / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 
 MEDIA_URL = '/media/'
